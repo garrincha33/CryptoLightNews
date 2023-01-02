3f9dfd83edac4b77980383a9e8ab63273d074cb2
Richy <garrincha33@users.noreply.github.com>
2023-01-02 18:41:49 +0000

    removing viewcontroller file

---
+++ CryptoLightNews/Constants/BaseConstant 2.swift
@@ -0,0 +1,29 @@
+//
+//  BaseConstant.swift
+//  CryptoLightNews
+//
+//  Created by Richard Price on 21/12/2022.
+//
+
+import Foundation
+import UIKit
+
+enum Constants {
+    
+    private static let resourceName = "ApiKey"
+    
+    private static var apiBase: NSDictionary {
+        guard let filePath = Bundle.main.path(forResource: resourceName, ofType: "plist"), let plist = NSDictionary(contentsOfFile: filePath) else {
+            fatalError("unable to find \(resourceName) plist")
+        }
+        return plist
+    }
+    
+    private static var API_KEY: String {
+        Constants.apiBase.object(forKey: "API_KEY") as? String ?? ""
+    }
+    
+    static let fullCrypto = "https://newsapi.org/v2/everything?q=crypto&sortBy=publishedAt&apiKey=\(Constants.API_KEY)"
+    
+    static let fullCoins = "https://api.coinlore.net/api/tickers/"
+}
---
+++ CryptoLightNews/Model/NewsArticles 2.swift
@@ -0,0 +1,20 @@
+//
+//  NewsArticles.swift
+//  CryptoLightNews
+//
+//  Created by Richard Price on 21/12/2022.
+//
+
+import Foundation
+
+struct NewsArticles: Codable {
+    var title: String?
+    var url: String?
+    var urlToImage: String?
+    var author: String?
+}
+struct SearchResults: Codable {
+    let status: String
+    let totalResults: Int
+    let articles: [NewsArticles]
+}
